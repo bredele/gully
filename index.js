df6e64e3f45ffb0288d5d7ae81ae710b963f9357
bredele <olivier.wietrich@gmail.com>
2013-10-30 18:48:28 -0600

    map events with on and delegate

--- index.js
+++ index.js
@@ -30,12 +30,15 @@ module.exports = Event;
 /**
  * Event plugin constructor
  * @param {Object} view event plugin scope
+ * @param {Boolean} isTouch optional
  * @api public
  */
 
 function Event(view, isTouch){
   this.view = view;
+  this.isTouch = isTouch || (window.ontouchstart !== undefined);
 }
+
 
 /**
  * Listen events.
@@ -48,7 +51,7 @@ function Event(view, isTouch){
 
 Event.prototype.on = function(node, type, callback, capture) {
   var _this = this;
-  ev.bind(node, type, function(e){
+  ev.bind(node, this.map(type), function(e){
     _this.view[callback].call(_this.view, e, node);
   }, (capture === 'true'));
 };
@@ -66,7 +69,18 @@ Event.prototype.on = function(node, type, callback, capture) {
 
 Event.prototype.delegate = function(node, selector, type, callback, capture) {
   var _this = this;
-  delegate.bind(node, selector, type, function(e){
+  delegate.bind(node, selector, this.map(type), function(e){
     _this.view[callback].call(_this.view, e, node);
   }, capture);
+};
+
+
+/**
+ * Map events (desktop and mobile)
+ * @param  {String} type event's name
+ * @return {String} mapped event
+ */
+
+Event.prototype.map = function(type) {
+	return this.isTouch ? (mapper[type] || type) : type;
 };
